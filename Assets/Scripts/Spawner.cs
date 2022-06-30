86813233550c144ec3a0a92d65b0e1e11334bbe0
Vitalii <vit.okushko@gmail.com>
2022-06-30 18:11:44 +0300

    Исправление по попытке №113320

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private int _countGameObject;
+    [SerializeField] private float _delaySeconds;
+    
+    private const float PositionAboveSpawnY = 5;
+
+    private void Start()
+    {
+        var createGameObjectJob = StartCoroutine(Create());
+    }
+
+    private IEnumerator Create()
+    {
+        var delay = new WaitForSeconds(_delaySeconds);
+        
+        for (int i = 0; i < _countGameObject; i++)
+        {
+            Instantiate(_prefab, new Vector3(transform.position.x, transform.position.y + PositionAboveSpawnY, transform.position.z), Quaternion.identity);
+            yield return delay;
+        }
+    }
+}
